ba89c467eb6d05aa1be689f74c67025446a43fc7
Saad Bazaz <saadbazaz@hotmail.com>
2021-11-19 13:08:00 +0500

    Add urdupython as a dependency instead of keeping a copy

--- setup.py
+++ setup.py
@@ -5,36 +5,20 @@ with open('README.rst') as f:
 
 import os 
 
-def copy_dir(path="./languages"):
-    dir_path = path
-    base_dir = os.path.join(path, dir_path)
-    for (dirpath, dirnames, files) in os.walk(base_dir):
-        for f in files:
-            yield os.path.join(dirpath.split('/', 1)[1], f)
-
-# folders_to_include = ["src", "languages", "samples"]
-
 setup(
     name='urdu_python',
     version='1.1',
-    packages=['urdu_python', 'urdu_python/src', 'urdu_python/src/filters'],
-    # package_data = {
-    #     '' : [f for f in copy_dir()]
-    # },
-    package_data={'':['languages/*/*.yaml']},
-    # package_data = {'languages': './languages'},
-    description='Simple example kernel for Jupyter',
+    packages=['urdu_python',],
+    description='UrduPython kernel for Jupyter',
     long_description=readme,
-    author='Jupyter Development Team',
-    author_email='jupyter@googlegroups.com',
+    author='Saad Bazaz',
+    author_email='saadbazaz@hotmail.com',
     url='https://github.com/saadbazaz/UrduPython',
     install_requires=[
-        'jupyter_client', 'IPython', 'ipykernel',
-
-        # Custom dependencies
-        'ply',
-        'PyYAML',
-        'Unidecode',
+        'jupyter_client', 
+        'IPython', 
+        'ipykernel',
+        'urdupython'
     ],
     classifiers=[
         'Intended Audience :: Developers',
